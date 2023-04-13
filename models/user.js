c00158b62348436a750c6adc69145589186f6304
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2023-04-13 13:43:06 +0530

    orders functionality

--- models/user.js
+++ models/user.js
@@ -30,7 +30,7 @@ class User{
 
   deleteItemFromCart(productId){
     const updatedCartItems=this.cart.items.filter(i=>{
-      return i.productId.toString()!=productId.toString()
+      return i.productId.toString()!==productId.toString()
     })
     const db=getDb();
     return db.collection('users').updateOne({_id:new mongodb.ObjectId(this._id)},{$set:{cart:{items:updatedCartItems}}})
@@ -49,6 +49,29 @@ class User{
                 }).quantity}
       })
     })
+  }
+
+  addOrder(){
+    const db=getDb();
+    return this.getCart().then(products=>{
+      const order={
+        items:products,
+        user:{
+          _id:new mongodb.ObjectId(this._id),
+          name:this.name
+        }
+      }
+      return db.collection('orders').insertOne(order)
+    })
+    .then(result=>{
+      this.cart={items:[]}
+      return db.collection('users').updateOne({_id:new mongodb.ObjectId(this._id)},{$set:{cart:{items:[]}}})
+    })
+  }
+
+  getOrders(){
+    const db=getDb();
+    return db.collection('orders').find({'user._id':new mongodb.ObjectId(this._id)}).toArray();
   }
 
   static findById(userId){
