2dfebb040e6aefb0a7553e330e3fd2019135d4d1
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2023-04-13 18:53:35 +0530

    user table

--- app.js
+++ app.js
@@ -12,20 +12,19 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-//const User = require('./models/user');
+const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('643701004721597afab38aa7')
-//     .then(user => {
-//       console.log(user)
-//       req.user = new User(user.name,user.email,user.cart,user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('6437fa8f5d48ef3b246e5cc9')
+    .then(user => {
+      req.user=user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -34,6 +33,18 @@ app.use(errorController.get404);
 
 mongoose.connect('mongodb+srv://dhruvbhanu1003:Dhruv2808@cluster0.0oqu9aj.mongodb.net/shop?retryWrites=true&w=majority')
   .then(result=>{
+    User.findOne().then(user=>{
+      if(!user){
+        const user=new User({
+          name:'dhruv',
+          email:'dhruv.bhanu.1003@gmail.com',
+          cart:{
+            items:[]
+          }
+        })
+        user.save();
+      }
+    })
     app.listen(3000);
   }).catch(err=>console.log(err))
 
--- controllers/admin.js
+++ controllers/admin.js
@@ -17,7 +17,8 @@ exports.postAddProduct = (req, res, next) => {
     title:title,
     price:price,
     description:description,
-    imageUrl:imageUrl
+    imageUrl:imageUrl,
+    userId:req.user
   });
   product.save()
     .then(result => {
@@ -73,7 +74,10 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.find()
+  //.select('title price -_id')
+  //.populate('userId')
     .then(products => {
+      console.log(products)
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
